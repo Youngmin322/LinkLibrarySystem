6dd16116b379f7359fe94110c6bc5731bcb04139
Youngmin Cho <choyoungmin0322@gmail.com>
2024-03-12 18:37:49 +0900

    2024.03.12

---
+++ src/main/java/com/LLS/LinkLibrarySystem/LinkLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.LLS.LinkLibrarySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LLS/LinkLibrarySystem/LinkLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LLS.LinkLibrarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
